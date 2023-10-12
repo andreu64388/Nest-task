29fd94d136aebe6af1236348ccac05a87099ac15
Andr_15_sh <90029729+andreu64388@users.noreply.github.com>
2023-10-12 17:16:37 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
+import * as process from 'process';
 const start = async () => {
   try {
     const app = await NestFactory.create(AppModule);
@@ -19,7 +19,7 @@ const start = async () => {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
-    await app.listen(5000);
+    await app.listen(process.env.PORT, '0.0.0.0');
   } catch (e) {
     console.log(e);
   }
